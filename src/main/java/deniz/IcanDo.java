9a2a9106dff587224437d30b6afb05fb719805dd
Sahin Ozsoy <sahin.ozsoy@gmail.com>
2021-02-17 14:38:59 -0500

    added some comment

--- src/main/java/deniz/IcanDo.java
+++ src/main/java/deniz/IcanDo.java
@@ -2,8 +2,10 @@ package deniz;
 
 public class IcanDo {
 
+    // This is a generic function that returns the sum of two integers
     public static int addition(int x, int y) {
 
+        // return x plus y
         return x+y;
     }
 
